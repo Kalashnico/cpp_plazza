5b93dadb719e36130c07e30ea0820973d00ec17c
Nicolas Guerin <nicolas.guerin@epitech.eu>
2018-04-17 13:43:08 +0200

    Created 4 basics classes required

---
+++ include/Processes.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Nicolas Guerin on 17/04/2018.
+//
+
+#pragma once
+
+class Processes {
+
+};
---
+++ src/plazza/UnixSockets.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicolas Guerin on 17/04/2018.
+//
+
+#include "../../include/UnixSockets.hpp"
