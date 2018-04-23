5f9a92005e6ae5767cb240113b70116a5af3efc0
Connor Gallimore <connor.gallimore@epitech.eu>
2018-04-23 15:25:35 +0200

    Started main logic

--- src/plazza/Plazza.cpp
+++ src/plazza/Plazza.cpp
@@ -5,4 +5,17 @@
 ** Main logic
 */
 
+#include <iostream>
 #include "Plazza.hpp"
+
+Plazza::Plazza()
+{}
+
+Plazza::~Plazza()
+{}
+
+void Plazza::sendCommandToSlaves(std::pair<std::string, Information> command)
+{
+	(void) command;
+	std::cout << "Command recieved: files: " << command.first << " enum type: " << command.second << std::endl;
+}
