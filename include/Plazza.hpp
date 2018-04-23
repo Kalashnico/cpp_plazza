bc52d67fe5c234b94bf6a2f9a655e5328c6899ce
Connor Gallimore <connor.gallimore@epitech.eu>
2018-04-23 12:43:48 +0200

    Structure changes

---
+++ include/Plazza.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** Plazza
+** File description:
+** Main logic
+*/
+
+#pragma once
+
+class Plazza {
+	public:
+		Plazza();
+		~Plazza();
+};
---
+++ src/plazza/Plazza.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2018
+** Plazza
+** File description:
+** Main logic
+*/
+
+#include "Plazza.hpp"
